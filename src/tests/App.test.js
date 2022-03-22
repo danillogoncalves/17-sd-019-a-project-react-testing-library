fb2e0ec5281a41852cb2dd8e68663bdda3952198
Danillo Gonçalves Batista <danillogoncalves001@gmail.com>
2022-03-22 16:10:17 -0300

    :heavy_check_mark: feat: Requisito 1 feito.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,36 @@
-test('', () => {});
+// test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('1. Teste o componente <App.js />', () => {
+  it(`Teste se o topo da aplicação contém um conjunto fixo de links de navegação,
+   e seus caminhos na URL.`,
+  () => {
+    const { history } = renderWithRouter(<App />);
+    const links = [
+      { link: 'Home', url: '/' },
+      { link: 'About', url: '/about' },
+      { link: 'Favorite Pokémons', url: '/favorites' }];
+    links.forEach(({ link, url }) => {
+      const nav = screen.getByRole('link', { name: link });
+      userEvent.click(nav);
+      const { location: { pathname } } = history;
+      expect(nav).toBeInTheDocument();
+      expect(pathname).toBe(url);
+    });
+  });
+
+  it('', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/agumom');
+    console.log(history);
+
+    const notFound = screen.getByRole('heading',
+      { name: /Page requested not found/i, level: 2 });
+    expect(notFound).toBeInTheDocument();
+    // const unknownURL
+  });
+});
