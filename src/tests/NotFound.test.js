365dc181bccafc54741561fc1524c549f9e2019d
Danillo Gonçalves Batista <danillogoncalves001@gmail.com>
2022-03-22 19:20:48 -0300

    :heavy_check_mark: feat: Requisito 4.

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import { NotFound } from '../components';
+import renderWithRouter from './renderWithRouter';
+
+describe('4. Teste o componente <NotFound.js />', () => {
+  it('', () => {
+    renderWithRouter(<NotFound />);
+    const text = screen
+      .getByRole('heading', { name: /Page requested not found/i, level: 2 });
+    expect(text).toBeInTheDocument();
+
+    const image = screen.getByAltText(
+      /Pikachu crying because the page requested was not found/i,
+    );
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
